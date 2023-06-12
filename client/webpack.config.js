151ecdec459c23d1fd3bdff43566336e2c0d51c7
lyskz <lyski.zach@icloud.com>
2023-06-12 12:23:24 -0700

    cleaned up

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -51,13 +51,13 @@ module.exports = () => {
           test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
         }, 
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
-        },
+        // {
+        //   test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        //   type: 'asset/resource',
+        // },
         {
           test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
+          exclude: /node_modules/,
           use: {
             loader: 'babel-loader',
             options: {
