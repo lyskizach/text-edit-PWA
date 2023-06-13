08d796443439964d2763071c310b1dcc9f3b5e98
lyskz <lyski.zach@icloud.com>
2023-06-13 12:57:32 -0700

    uncommented n unified in webpack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -50,11 +50,7 @@ module.exports = () => {
         {
           test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
-        }, 
-        // {
-        //   test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        //   type: 'asset/resource',
-        // },
+        },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
